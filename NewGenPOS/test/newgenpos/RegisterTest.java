cec82567646d349d1cd3685b6758fe8bdd903170
willydk <willydk@gmail.com>
2012-12-04 14:20:51 -0800

    Update NewGenPOS/test/newgenpos/RegisterTest.java

--- NewGenPOS/test/newgenpos/RegisterTest.java
+++ NewGenPOS/test/newgenpos/RegisterTest.java
@@ -1,18 +1,19 @@
 /*
- * HW5, Tests Not Done, need to rewrite the register methods to not interact with the UI
+ * HW5, Testing on Cash, Credit, Check payment
  */
 package newgenpos;
 
+import com.trolltech.qt.gui.QDialog;
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
- * @author Mike
+ * @author Willy 2
  */
 public class RegisterTest {
     private int salesNumber = 1;
@@ -20,9 +21,12 @@ public class RegisterTest {
     private String name = "testName";
     private ProductCatalog catalog;
     private int storeID = 1;
+    String input;
     
-    private Payment payment;
-    private Register register;
+    private Register register; 
+    private Money paymentAmount;
+    private Money total = new Money(5.00);
+    private Sale currentSale;
     
     public RegisterTest() {
     }
@@ -45,43 +49,71 @@ public class RegisterTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of makeCashPayment method, of class Register.
-     */
+    public boolean makeCashPayment(){        
+        boolean result = false;
+        input = "10.00";
+        boolean success = checkAmount(input);
+        if(success)
+            result = true;
+        else 
+            result = false;
+        return  result;     
+    }
+    private boolean checkAmount(String input){
+        try{
+            Double paymentInput = Double.parseDouble(input);
+            this.paymentAmount = new Money(paymentInput);
+            //Ensure payment is >= the total
+            if(this.paymentAmount.checkTotal(this.total)){
+                return true;
+            }
+            else{
+                Ui_NewGenPOS.setText("Insufficient Funds!");
+                return false;
+            }
+        }
+        catch(NumberFormatException e){
+            Ui_NewGenPOS.setText("Payment amount must contain ONLY numbers and must NOT be blank! Try Again!");
+            return false;
+        }
+    }
+    
     @Test
     public void testMakeCashPayment() {
-        System.out.println("Testing: makeCashPayment");
-        Register instance = this.register;
+        System.out.println("Testing Method: makeCashPayment");
         boolean expResult = true;
-        boolean result = instance.makeCashPayment();
+        boolean result = makeCashPayment();
         assertEquals(expResult, result);
+        if(expResult == result)
+        {
+            System.out.println("Test case passed for makeCashPayment");
+        }
     }
+
+    
 
     /**
      * Test of makeCreditPayment method, of class Register.
-     */
+     
     @Test
     public void testMakeCreditPayment() {
-        System.out.println("makeCreditPayment");
-        Register instance = null;
+        System.out.println("Testing Method: makeCreditPayment");
+        Register instance = this.register;
         boolean expResult = false;
         boolean result = instance.makeCreditPayment();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of makeCheckPayment method, of class Register.
-     */
+     
     @Test
     public void testMakeCheckPayment() {
-        System.out.println("makeCheckPayment");
-        Register instance = null;
+        System.out.println("Testing Method: makeCheckPayment");
+        Register instance = this.register;
         boolean expResult = false;
         boolean result = instance.makeCheckPayment();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    }*/
+
 }
